877d9a2a61a4c3e002a99a2cadbc9622d1384eb0
psegarrac <94398207+psegarrac@users.noreply.github.com>
2021-12-21 22:58:22 +0100

    Conexión firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,18 @@
 
-
 export const environment = {
-  production: false
+  
+
+  production: false,
+  firebase: {
+    projectId: 'spax-90789',
+    appId: '1:834858182317:web:e350c33e36d6a787c68824',
+    databaseURL: 'https://spax-90789-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'spax-90789.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCjF-zrcTKQ3-iQcHThiV7swpT6JIqk6u8',
+    authDomain: 'spax-90789.firebaseapp.com',
+    messagingSenderId: '834858182317',
+    measurementId: 'G-XYL81JMXR4',
+}
+
 };
-
-
